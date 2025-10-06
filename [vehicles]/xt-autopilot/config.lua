9fe7a4af8b3513fdeef697ca05a8e07dc60a80d6
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-07 00:42:25 +0100

    Autopilot

---
+++ [vehicles]/xt-autopilot/config.lua
@@ -0,0 +1,10 @@
+return {
+    keybind = 'O',                  -- Autopilot keybind
+    useMPH = true,                  -- Use MPH
+    stopAutoPilotDistance = 80,     -- Distance from waypoint when autopilot disables
+    driveStyles = {                 -- Autopilot drive styles || https://gtaforums.com/topic/822314-guide-driving-styles/
+        { label = 'Normal', value = 786603 },
+        { label = 'Rushed', value = 1074528293 },
+    },
+    allowedClasses = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 17, 18, 22 },
+}
