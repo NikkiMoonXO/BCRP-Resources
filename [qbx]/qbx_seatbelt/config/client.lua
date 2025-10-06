bd8a4e0b74e672006e0c6b1df1afeb3f72cc4c52
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 16:15:07 +0100

    Seatbelt Changes

--- [qbx]/qbx_seatbelt/config/client.lua
+++ [qbx]/qbx_seatbelt/config/client.lua
@@ -1,8 +1,8 @@
 return {
     keybind = 'B', -- Keybind to toggle seatbelt (https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/)
-	useMPH = false, -- Use MPH instead of KMH
-	minSpeedUnbuckled = 20.0, -- Minimum speed to fly through windscreen when seatbelt is off
-	minSpeedBuckled = 160.0, -- Minimum speed to fly through windscreen when seatbelt is on
+	useMPH = true, -- Use MPH instead of KMH
+	minSpeedUnbuckled = 120.0, -- Minimum speed to fly through windscreen when seatbelt is off
+	minSpeedBuckled = 400.0, -- Minimum speed to fly through windscreen when seatbelt is on
     harness = {
         disableFlyingThroughWindscreen = true, -- Disable flying through windscreen when harness is on
         minSpeed = 200.0 -- If the above is set to false, minimum speed to fly through windscreen when harness is on
