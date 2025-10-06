707cd190cb19e7a921d7e1a5903972d5f297a34f
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 12:01:04 +0100

    Mechanic Shop MLOs

---
+++ [assets]/did_paleto_garage/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+version '1.0.0'
+this_is_a_map 'yes'
+games { 'gta5' }
+
+escrow_ignore {
+  'stream/gamefiles/hi/*.*',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'm23_2_mp2023_02_dlc_int_4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'sf_dlc_int_02_sec.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'sm_smugdlc_int_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_int_63.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bkr_biker_dlc_int_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'lr_supermod_garage_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'ba_int_02_ba.ytyp' 
+data_file 'DLC_ITYP_REQUEST' 'v_int_73.ytyp'
+
+
+files {
+  "audio/did_midnight_paleto_garage_game.dat151.rel"
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/did_midnight_paleto_garage_game.dat'
+dependency '/assetpacks'
