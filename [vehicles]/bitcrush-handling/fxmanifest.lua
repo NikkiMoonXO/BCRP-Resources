56da3de551eef2e1855e2c532e19280300fa6783
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 23:30:12 +0100

    Car Handling

---
+++ [vehicles]/bitcrush-handling/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+name 'PRP Lore Handling'
+
+files {
+    'data/**/handling.meta',
+    'carcols.meta',
+    'carvariations.meta',
+	'popgroups.ymt'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
