634e4667e2fdba5b3834f39dba610aab7dd5c9f5
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 14:33:14 +0100

    Combat Walk

---
+++ [standalone]/brp-stopcombatwalk/client.lua
@@ -0,0 +1,16 @@
+Citizen.CreateThread( function()
+
+
+    while true do
+      
+      Citizen.Wait(0)
+      local ped = GetPlayerPed(-1)
+      if not GetPedConfigFlag(ped,78,1) then
+      
+        SetPedUsingActionMode(GetPlayerPed(-1), false, -1, 0)
+
+      end
+
+    end
+
+end)
---
+++ [standalone]/brp-stopcombatwalk/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bluster_Madness'
+version '1.0'
+
+-- Add a client script 
+client_script 'client.lua'
