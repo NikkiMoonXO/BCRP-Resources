0b545d6da9903e286ce48a2253e49025cb9ef684
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 17:29:55 +0100

    Revert "Vehicle Control"
    This reverts commit 4f3fc98e8ffb2fad8661104aad3db9d618c0f8ac.

--- [standalone]/sd-vehcontrol/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-Config.Debug = false -- enable/disable debug prints for the UI
-
-Config.CommandName = "veh" -- Command to open the UI
-Config.KeyBind = "F7" -- Keybind to open the UI
