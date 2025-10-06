4f3fc98e8ffb2fad8661104aad3db9d618c0f8ac
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 17:13:46 +0100

    Vehicle Control

---
+++ [standalone]/sd-vehcontrol/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.Debug = false -- enable/disable debug prints for the UI
+
+Config.CommandName = "veh" -- Command to open the UI
+Config.KeyBind = "F7" -- Keybind to open the UI
