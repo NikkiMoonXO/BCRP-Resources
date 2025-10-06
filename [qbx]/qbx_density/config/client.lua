4c55324c978972dacba0f2bcf6198a740b52348a
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 16:19:20 +0100

    Changed Density of Vehicles

--- [qbx]/qbx_density/config/client.lua
+++ [qbx]/qbx_density/config/client.lua
@@ -4,7 +4,7 @@ return {
     -- Value of 1.0 represents GTA Online populate rates
     -- Value of 0.0 removes all of that type
     parked = 0.8, -- Density of parked vehicles
-    vehicle = 0.8, -- Density of vehicles
+    vehicle = 0.6, -- Density of vehicles
     randomvehicles = 0.8, -- Density of random vehicles
     peds = 0.8, -- Density of random peds (civilians, etc.)
     scenario = 0.8, -- Density of scenario peds (bikers, gangsters, etc.)
