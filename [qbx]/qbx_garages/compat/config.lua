a1c589f43c25005c18690145cd563678e907f98c
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 19:58:39 +0100

    Garages Change

--- [qbx]/qbx_garages/compat/config.lua
+++
@@ -1,7 +0,0 @@
-local Garages = require 'config.shared'.Garages
-local HouseGarages = require 'config.shared'.HouseGarages
-
-Config = {}
-
-Config.HouseGarages = HouseGarages
-Config.Garages = Garages
