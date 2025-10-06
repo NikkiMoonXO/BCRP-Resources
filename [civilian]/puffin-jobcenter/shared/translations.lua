0db86f89968c801fef4509dd32046974a5ca1b39
Haywood90 <149424144+Haywood90@users.noreply.github.com>
2025-10-06 18:22:17 +0100

    Job Center UI

---
+++ [civilian]/puffin-jobcenter/shared/translations.lua
@@ -0,0 +1,8 @@
+Lang = {
+    job_error = 'Invalid job selected!', 
+    coords_error = 'No coordinates set for this job!', 
+    job_set = 'Your job has been set To %s.',
+    target_label = 'Browse Jobs',
+    cmd_suggestion = 'Open Job Center',
+    coords_set = 'Waypoint Set To %s Job.',
+}
